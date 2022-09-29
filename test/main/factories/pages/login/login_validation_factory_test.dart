fd186750add0fab514a1149d91eecde9b575959c
Elberte Plínio <elbertedoolan@gmail.com>
2022-09-29 15:10:30 -0300

    Ensure LoginValidationsFactory returns correct validations

---
+++ test/main/factories/pages/login/login_validation_factory_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+
+import 'package:polls/validation/validators/validators.dart';
+import 'package:polls/main/factories/factories.dart';
+
+void main() {
+  test('Should return the correct validations', () {
+    final validations = makeLoginValidations();
+
+    expect(validations, [
+      RequiredFieldValidation('email'),
+      EmailValidation('email'),
+      RequiredFieldValidation('password'),
+    ]);
+  });
+}
