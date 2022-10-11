6cb7aeaf7c17992703fcfbd14801e44905cfeb9f
Elberte Plínio <elbertedoolan@gmail.com>
2022-10-10 21:10:17 -0300

    Add GetxLoginPresenter

--- lib/main/factories/pages/login/login_page_factory.dart
+++ lib/main/factories/pages/login/login_page_factory.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../../../../ui/pages/pages.dart';
 import '../../factories.dart';
 
-
 Widget makeLoginPage() {
-  return LoginPage(makeLoginPresenter());
+  return LoginPage(makeGetxLoginPresenter());
 }
