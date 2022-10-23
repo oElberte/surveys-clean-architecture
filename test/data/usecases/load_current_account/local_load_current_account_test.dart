6de120a543506085d9a90095ac5278a51484a2e8
Elberte Plinio <elbertedoolan@gmail.com>
2022-10-23 15:22:30 -0300

    Move production code to their own files

--- test/data/usecases/load_current_account/local_load_current_account_test.dart
+++ test/data/usecases/load_current_account/local_load_current_account_test.dart
@@ -1,30 +1,12 @@
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
-import 'package:polls/domain/helpers/helpers.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
+import 'package:polls/domain/helpers/helpers.dart';
 import 'package:polls/domain/entities/entities.dart';
-import 'package:polls/domain/usecases/usecases.dart';
 
-class LocalLoadCurrentAccount implements LoadCurrentAccount {
-  final FetchSecureCacheStorage fetchSecureCacheStorage;
-
-  LocalLoadCurrentAccount({@required this.fetchSecureCacheStorage});
-
-  Future<AccountEntity> load() async {
-    try {
-      final token = await fetchSecureCacheStorage.fetchSecure('token');
-      return AccountEntity(token);
-    } catch (error) {
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class FetchSecureCacheStorage {
-  Future<String> fetchSecure(String key);
-}
+import 'package:polls/data/usecases/usecases.dart';
+import 'package:polls/data/cache/cache.dart';
 
 class FetchSecureCacheStorageSpy extends Mock
     implements FetchSecureCacheStorage {}
