9e766113df97d94be3a40cf07e3425378425de8c
Elberte Plinio <elbertedoolan@gmail.com>
2022-10-24 18:48:12 -0300

    Add SplashPage factories

---
+++ lib/main/factories/pages/splash/splash_page_factory.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../../../../ui/pages/pages.dart';
+import '../../factories.dart';
+
+Widget makeSplashPage() {
+  return SplashPage(
+    presenter: makeGetxSplashPresenter(),
+  );
+}
--- lib/main/main.dart
+++ lib/main/main.dart
@@ -18,10 +18,23 @@ class App extends StatelessWidget {
       title: '4Dev',
       debugShowCheckedModeBanner: false,
       theme: makeAppTheme(),
-      initialRoute: '/login',
+      initialRoute: '/',
       getPages: [
-        GetPage(name: '/login', page: makeLoginPage),
-        GetPage(name: '/surveys', page: () => Scaffold(body: Text('Polls'))),
+        GetPage(
+          name: '/',
+          page: makeSplashPage,
+          transition: Transition.fade,
+        ),
+        GetPage(
+          name: '/login',
+          page: makeLoginPage,
+          transition: Transition.fadeIn,
+        ),
+        GetPage(
+          name: '/surveys',
+          page: () => Scaffold(body: Text('Polls')),
+          transition: Transition.fadeIn,
+        ),
       ],
     );
   }
