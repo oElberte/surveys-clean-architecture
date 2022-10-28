9e805d108a4fb3b5c5d0e0b3403e080ea24b9be8
Elberte Plinio <elbertedoolan@gmail.com>
2022-10-28 19:42:01 -0300

    Ensure GetxSignUpPresnter emits correct error on email changed

---
+++ test/presentation/presenters/getx_signup_presenter_test.dart
@@ -0,0 +1,74 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:polls/ui/helpers/errors/errors.dart';
+
+import 'package:polls/presentation/presenters/presenters.dart';
+import 'package:polls/presentation/protocols/protocols.dart';
+
+class ValidationSpy extends Mock implements Validation {}
+
+void main() {
+  GetxSignUpPresenter sut;
+  ValidationSpy validation;
+  String email;
+
+  PostExpectation mockValidationCall(String field) => when(validation.validate(
+      field: field == null ? anyNamed('field') : field,
+      value: anyNamed('value')));
+
+  void mockValidation({String field, ValidationError value}) {
+    mockValidationCall(field).thenReturn(value);
+  }
+
+  setUp(() {
+    validation = ValidationSpy();
+    sut = GetxSignUpPresenter(
+      validation: validation,
+    );
+    email = faker.internet.email();
+    mockValidation();
+  });
+
+  test('Should call Validation with correct email', () {
+    sut.validateEmail(email);
+
+    verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+
+  test('Should emit invalid field error if email is invalid', () {
+    mockValidation(value: ValidationError.invalidField);
+
+    //Uses the value inside ".listen" and calls "expectAsyn1" with this value directly
+    sut.emailErrorStream
+        .listen(expectAsync1((error) => expect(error, UIError.invalidField)));
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
+
+  test('Should emit required field error if email is empty', () {
+    mockValidation(value: ValidationError.requiredField);
+
+    //Uses the value inside ".listen" and calls "expectAsyn1" with this value directly
+    sut.emailErrorStream
+        .listen(expectAsync1((error) => expect(error, UIError.requiredField)));
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
+
+  test('Should emit null if email validation succeeds', () {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
+}
