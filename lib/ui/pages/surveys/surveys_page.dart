caf266288b53752940f8c5de28b2644241fa69a1
Elberte Plinio <elbertedoolan@gmail.com>
2022-11-09 15:56:24 -0300

    Ensure SurveysPage calls loadData on page load

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -3,10 +3,17 @@ import 'package:flutter/material.dart';
 
 import '../../helpers/helpers.dart';
 import 'components/components.dart';
+import 'surveys_presenter.dart';
 
 class SurveysPage extends StatelessWidget {
+  final SurveysPresenter presenter;
+
+  SurveysPage(this.presenter);
+
   @override
   Widget build(BuildContext context) {
+    presenter.loadData();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(R.string.surveys),
