c26c756e8d8e2085c0652c007294ff0e94cd1572
Elberte Plinio <elbertedoolan@gmail.com>
2022-11-08 21:36:42 -0300

    Ensure RemoteLoadSurveys throws AccessDeniedError on 403

--- test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -21,8 +21,10 @@ class RemoteLoadSurveys {
       return httpResponse
           .map((json) => RemoteSurveyModel.fromJson(json).toEntity())
           .toList();
-    } on HttpError {
-      throw DomainError.unexpected;
+    } on HttpError catch (error) {
+      throw error == HttpError.forbbiden
+          ? DomainError.accessDenied
+          : DomainError.unexpected;
     }
   }
 }
@@ -124,5 +126,13 @@ void main() {
     final future = sut.load();
 
     expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw AccessDeniedError if HttpClient returns 403', () async {
+    mockHttpError(HttpError.forbbiden);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.accessDenied));
   });
 }
