dd05efe0a1a4ec2c94276d77dd3d82396908f9d9
Elberte Plinio <elbertedoolan@gmail.com>
2022-11-10 21:43:44 -0300

    Move GetxSurveysPresenter to its own file

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Polls
-Polls for developers, app for training.
+# Surveys
+Surveys for developers, app for learning and training.
 
 ## Getting started
 The app is made on Flutter version 1.20.4 using FVM for version management, when the app is finished will be updated to the latest Flutter version.
--- test/presentation/presenters/getx_surveys_presenter_test.dart
+++ test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -1,47 +1,14 @@
 import 'package:faker/faker.dart';
-import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 import 'package:polls/domain/helpers/helpers.dart';
+import 'package:polls/presentation/presenters/presenters.dart';
 import 'package:polls/ui/helpers/errors/errors.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 
 import 'package:polls/domain/entities/entities.dart';
 import 'package:polls/domain/usecases/usecases.dart';
 
 import 'package:polls/ui/pages/pages.dart';
-
-class GetxSurveysPresenter {
-  final LoadSurveys loadSurveys;
-
-  final _isLoading = true.obs;
-  final _surveys = Rx<List<SurveyViewModel>>();
-
-  Stream<bool> get isLoadingStream => _isLoading.stream;
-  Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
-
-  GetxSurveysPresenter({@required this.loadSurveys});
-
-  Future<void> loadData() async {
-    try {
-      _isLoading.value = true;
-      final surveys = await loadSurveys.load();
-      _surveys.value = surveys
-          .map((survey) => SurveyViewModel(
-                id: survey.id,
-                question: survey.question,
-                date: DateFormat('dd MMM yyyy').format(survey.date),
-                didAnswer: survey.didAnswer,
-              ))
-          .toList();
-    } on DomainError {
-      _surveys.subject.addError(UIError.unexpected.description);
-    } finally {
-      _isLoading.value = false;
-    }
-  }
-}
 
 class LoadSurveysSpy extends Mock implements LoadSurveys {}
 
