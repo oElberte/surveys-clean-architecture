2544c07cbbeca38f3164d78cd22b803d7c4d055d
Elberte Plinio <elbertedoolan@gmail.com>
2022-11-14 19:29:41 -0300

    Add AuthorizeHttpClientDecorator factory

--- lib/main/factories/usecases/load_surveys_factory.dart
+++ lib/main/factories/usecases/load_surveys_factory.dart
@@ -2,10 +2,9 @@ import '../../../data/usecases/usecases.dart';
 import '../../../domain/usecases/usecases.dart';
 import '../factories.dart';
 
-
 LoadSurveys makeRemoteLoadSurveys() {
   return RemoteLoadSurveys(
-    httpClient: makeHttpAdapter(),
+    httpClient: makeAuthorizeHttpClientDecorator(),
     url: makeApiUrl('surveys'),
   );
 }
