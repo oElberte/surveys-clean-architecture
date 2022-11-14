7b22c79930860659fc0509e7bc58ff08a24b06b8
Elberte Plinio <elbertedoolan@gmail.com>
2022-11-14 20:26:34 -0300

    Ensure LocalLoadSurveys returns a list of Surveys on success

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -1,5 +1,8 @@
+import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
+import 'package:surveys/data/models/models.dart';
+import 'package:surveys/domain/entities/entities.dart';
 import 'package:test/test.dart';
 
 class LocalLoadSurveys {
@@ -7,31 +10,75 @@ class LocalLoadSurveys {
 
   LocalLoadSurveys({@required this.fetchCacheStorage});
 
-  Future<void> load() async {
-    await fetchCacheStorage.fetch('surveys');
+  Future<List<SurveyEntity>> load() async {
+    final data = await fetchCacheStorage.fetch('surveys');
+    return data
+        .map<SurveyEntity>((json) => LocalSurveyModel.fromJson(json).toEntity())
+        .toList();
   }
 }
 
 class FetchCacheStorageSpy extends Mock implements FetchCacheStorage {}
 
 abstract class FetchCacheStorage {
-  Future<void> fetch(String key);
+  Future<dynamic> fetch(String key);
 }
 
 void main() {
-  FetchCacheStorageSpy fetchCacheStorage;
   LocalLoadSurveys sut;
+  FetchCacheStorageSpy fetchCacheStorage;
+  List<Map> data;
+
+  List<Map> mockValidData() => [
+        {
+          'id': faker.guid.guid(),
+          'question': faker.randomGenerator.string(10),
+          'date': '2022-11-14T00:00:00Z',
+          'didAnswer': 'false',
+        },
+        {
+          'id': faker.guid.guid(),
+          'question': faker.randomGenerator.string(10),
+          'date': '2021-02-02T00:00:00Z',
+          'didAnswer': 'true',
+        },
+      ];
+
+  void mockFetch(List<Map> list) {
+    data = list;
+    when(fetchCacheStorage.fetch(any)).thenAnswer((_) async => data);
+  }
 
   setUp(() {
     fetchCacheStorage = FetchCacheStorageSpy();
     sut = LocalLoadSurveys(
       fetchCacheStorage: fetchCacheStorage,
     );
+    mockFetch(mockValidData());
   });
 
   test('Should call FetchCacheStorage with correct key', () async {
     await sut.load();
 
     verify(fetchCacheStorage.fetch('surveys')).called(1);
+  });
+
+  test('Should return a list of surveys on success', () async {
+    final surveys = await sut.load();
+
+    expect(surveys, [
+      SurveyEntity(
+        id: data[0]['id'],
+        question: data[0]['question'],
+        date: DateTime.utc(2022, 11, 14),
+        didAnswer: false,
+      ),
+      SurveyEntity(
+        id: data[1]['id'],
+        question: data[1]['question'],
+        date: DateTime.utc(2021, 02, 02),
+        didAnswer: true,
+      ),
+    ]);
   });
 }
