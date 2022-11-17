855858b5b6b33dfe31dc94c83b5628e37dd03043
Elberte Plinio <elbertedoolan@gmail.com>
2022-11-17 14:53:29 -0300

    Move LocalStorageAdapter to its own file

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,19 +1,8 @@
 import 'package:faker/faker.dart';
 import 'package:localstorage/localstorage.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
+import 'package:surveys/infra/cache/cache.dart';
 import 'package:test/test.dart';
-
-class LocalStorageAdapter {
-  final LocalStorage localStorage;
-
-  LocalStorageAdapter({@required this.localStorage});
-
-  Future<void> save({@required String key, @required dynamic value}) async {
-    await localStorage.deleteItem(key);
-    await localStorage.setItem(key, value);
-  }
-}
 
 class LocalStorageSpy extends Mock implements LocalStorage {}
 
