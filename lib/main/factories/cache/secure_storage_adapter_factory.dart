7d63a1fff4e1ec97dbbccca4a3c6f266d737ee25
Elberte Plinio <elbertedoolan@gmail.com>
2022-11-17 17:53:39 -0300

    Add RemoteLoadSurveysWithLocalFallback factory

--- lib/main/factories/cache/secure_storage_adapter_factory.dart
+++ lib/main/factories/cache/secure_storage_adapter_factory.dart
@@ -3,6 +3,5 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import '../../../infra/cache/secure_storage_adapter.dart';
 
 SecureStorageAdapter makeSecureStorageAdapter() {
-  final secureStorage = FlutterSecureStorage();
-  return SecureStorageAdapter(secureStorage: secureStorage);
+  return SecureStorageAdapter(secureStorage: FlutterSecureStorage());
 }
