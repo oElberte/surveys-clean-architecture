254e71f68df4c5bc39299cacac2a9d2e28192534
Elberte Plinio <elbertedoolan@gmail.com>
2022-11-17 17:30:04 -0300

    Ensure Composite rethrows if remote load throws AccessDeniedError

--- test/main/composites/remote_load_surveys_with_local_fallback_test.dart
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -1,6 +1,7 @@
 import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
+import 'package:surveys/domain/helpers/helpers.dart';
 import 'package:test/test.dart';
 import 'package:surveys/domain/usecases/usecases.dart';
 import 'package:surveys/domain/entities/entities.dart';
@@ -41,10 +42,15 @@ void main() {
         )
       ];
 
+  PostExpectation mockRemoteLoadCall() => when(remote.load());
+
   void mockRemoteLoad() {
     remoteSurveys = mockSurveys();
-    when(remote.load()).thenAnswer((_) async => remoteSurveys);
+    mockRemoteLoadCall().thenAnswer((_) async => remoteSurveys);
   }
+
+  void mockRemoteLoadError(DomainError error) =>
+      mockRemoteLoadCall().thenThrow(error);
 
   setUp(() {
     remote = RemoteLoadSurveysSpy();
@@ -72,5 +78,13 @@ void main() {
     final surveys = await sut.load();
 
     expect(surveys, remoteSurveys);
+  });
+
+  test('Should rethrow if remote load throws AccessDeniedError', () async {
+    mockRemoteLoadError(DomainError.accessDenied);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.accessDenied));
   });
 }
