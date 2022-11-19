d4508a25045a68280388d519350a544b10ee2853
Elberte Plinio <elbertedoolan@gmail.com>
2022-11-18 21:02:07 -0300

    Add GetxSurveyResultPresenter with tests

--- lib/ui/pages/survey_result/survey_answer_viewmodel.dart
+++ lib/ui/pages/survey_result/survey_answer_viewmodel.dart
@@ -1,15 +1,19 @@
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
-class SurveyAnswerViewModel {
+class SurveyAnswerViewModel extends Equatable {
   final String image;
   final String answer;
   final bool isCurrentAnswer;
   final String percent;
 
+  List get props => ['image', 'answer', 'isCurrentAnswer', 'percent'];
+  
   SurveyAnswerViewModel({
     this.image,
     @required this.answer,
     @required this.isCurrentAnswer,
     @required this.percent,
   });
+
 }
