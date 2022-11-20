4356090432986b39db6cd3faa18c1372ee35e798
Elberte Plinio <elbertedoolan@gmail.com>
2022-11-20 20:21:42 -0300

    Fix bug on save survey result

---
+++ lib/presentation/helpers/survey_result_entity_extensions.dart
@@ -0,0 +1,23 @@
+import '../../domain/entities/entities.dart';
+import '../../ui/pages/pages.dart';
+
+extension SurveyResultEntityExtensions on SurveyResultEntity {
+  SurveyResultViewModel toViewModel() {
+    return SurveyResultViewModel(
+      surveyId: surveyId,
+      question: question,
+      answers: answers.map((answer) => answer.toViewModel()).toList(),
+    );
+  }
+}
+
+extension SurveyAnswerEntityExtensions on SurveyAnswerEntity {
+  SurveyAnswerViewModel toViewModel() {
+    return SurveyAnswerViewModel(
+      image: image,
+      answer: answer,
+      isCurrentAnswer: isCurrentAnswer,
+      percent: '$percent%',
+    );
+  }
+}
