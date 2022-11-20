85879abd1a3feb3212f0e55ffeded96a06fac42e
Elberte Plinio <elbertedoolan@gmail.com>
2022-11-20 18:22:09 -0300

    Add SaveSurveyResult use case

--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -2,5 +2,6 @@ export './authentication.dart';
 export './create_account.dart';
 export './load_current_account.dart';
 export './load_survey_result.dart';
+export './save_survey_result.dart';
 export './load_surveys.dart';
 export './save_current_account.dart';
